392ac2fc882367d2ac56f550827b25f0f29e3937
MeeLeSh <teadorWOT@gmail.com>
2024-04-06 21:28:51 +0300

    module2-task1

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import MainScreen from '../pages/main-screen/main-screen';
+
+type AppScreenProps = {
+  cardsNumber: number;
+};
+
+function App({cardsNumber}: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen cardsNumber = {cardsNumber}/>
+  );
+}
+
+export default App;
